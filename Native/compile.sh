1002b437e60e72d848e9a78f08fd95c6309151aa
Bembe <pozder.dario@gmail.com>
2025-03-13 10:33:39 +0100

    cosmetic

--- Native/compile.sh
+++
@@ -1,8 +0,0 @@
- # Linux
-#gcc win.c -lX11 -lasound -o warp && ./warp
-# macOS
-#cc win.c -framework Cocoa -framework AudioToolbox -o warp && ./warp
-# windows
-
-#note the backslash differnce in path for execution
-gcc src/win.c -lgdi32 -lwinmm -o dist/warp.exe && dist\warp.exe
