3ad28080c14eee02ae0949bfa13a1d1bc861e17c
Bembe <pozder.dario@gmail.com>
2025-05-26 21:40:39 +0200

    cosmetic

--- Native/src/web.cpp
+++
@@ -1,9 +0,0 @@
-//a web class that the api uses to create a web
-//for start we dont need this, we can use api to create a webview class
-//methods :
-
-/*   
-
-
-
-*/
--- Native/src/win.cpp
+++
@@ -1,7 +0,0 @@
-//a win class that the api uses to create a win
-//for start we dont need this, we can use api to create a fenster class
-class Win {
-
-
-
-};
